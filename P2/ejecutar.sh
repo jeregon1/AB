45081763bfee581d7b8707038376258ca9be16b6
jeregon <a839922@unizar.es>
2024-02-23 19:21:29 +0100

    Better ejecutar.sh

--- P2/ejecutar.sh
+++ P2/ejecutar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Script de pruebas de busca.py
+
+# Si se pasan dos argumentos, se ejecuta el script con esos argumentos
+# Si no, se ejecutan las pruebas con los archivos de prueba
+
 if [ $# -eq 2 ]; then
     python busca.py $1 $2
     exit 0
@@ -14,6 +19,11 @@ fich_3="3_moreArticles.txt"
 echo "Pruebas con los archivos de prueba"
 
 # Pruebo con los archivos de prueba
-python3 ../busca.py fich_1 "res_$fich_1"
-python3 ../busca.py fich_2 "res_$fich_2"
-python3 ../busca.py fich_3 "res_$fich_3"
+echo Fichero: $fich_1; echo
+python3 ../busca.py $fich_1 "res_$fich_1"
+
+echo Fichero: $fich_2; echo
+python3 ../busca.py $fich_2 "res_$fich_2"
+
+echo Fichero: $fich_3; echo
+python3 ../busca.py $fich_3 "res_$fich_3"
