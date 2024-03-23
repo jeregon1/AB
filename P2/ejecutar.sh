644c417bf8b44bfb460895b1311fadb467966f96
jeregon <a839922@unizar.es>
2024-03-23 17:43:30 +0100

    better P2 ejecutar.sh

--- P2/ejecutar.sh
+++ P2/ejecutar.sh
@@ -6,7 +6,7 @@
 # Si no, se ejecutan las pruebas con los archivos de prueba
 
 if [ $# -eq 1 ]; then
-    python3 busca.py pruebas/$1 pruebas/res_$1
+    python3 busca.py pruebas/$1 pruebas/res_$1 && cat pruebas/res_$1
     exit 0
 fi
 
@@ -19,18 +19,11 @@ fich_4="4_tricky.txt"
 
 echo "Pruebas con los archivos de prueba"
 
-# Pruebo con los archivos de prueba
-python3 ../busca.py $fich_1 "res_$fich_1"
-echo Fichero $fich_1 finalizado
-
-python3 ../busca.py $fich_2 "res_$fich_2"
-echo Fichero $fich_2 finalizado
-
-python3 ../busca.py $fich_3 "res_$fich_3"
-echo Fichero $fich_3 finalizado
-
-python3 ../busca.py $fich_4 "res_$fich_4"
-echo Fichero $fich_4 finalizado
+for f in $fich_1 $fich_2 $fich_3 $fich_4; do
+    echo "Probando con $f"
+    python3 ../busca.py $f "res_$f"
+    cat "res_$f"
+done
 
 echo Tests:
 echo
